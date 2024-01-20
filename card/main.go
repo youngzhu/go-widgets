c0749b09d33bb564a20d69b7a65fa010d9199431
Young-Mac Pro <young.zhu.2012@gmail.com>
2024-01-20 16:38:55 +0800

    每周五添加下周的，方便计划下周的工作安排

--- card/main.go
+++ card/main.go
@@ -10,9 +10,14 @@ import (
 
 func main() {
 	today := godate.Today()
-	workdays := today.Workdays()
-	monday := workdays[0]
-	friday := workdays[4]
+	// 每周五添加下周的，方便计划下周的工作安排
+	//if today.Weekday() != godate.Friday {
+	//	log.Println("今天不是星期五")
+	//	os.Exit(0)
+	//}
+
+	monday := today.NextWorkday()
+	friday, _ := monday.AddDay(4)
 
 	title := monday.String() + " ~ " + friday.String()
 	err := basecamp.CreateCard("Profession", "Card Table", "In progress",
