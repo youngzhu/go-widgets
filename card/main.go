364bb2c9dde2ace1f636a68ae92e8c2b5817b320
Young-Mac Pro <young.zhu.2012@gmail.com>
2024-01-20 16:42:03 +0800

    每周五添加下周的，方便计划下周的工作安排

--- card/main.go
+++ card/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/youngzhu/go-basecamp"
 	"github.com/youngzhu/godate"
 	"log"
+	"os"
 )
 
 // basecamp card table
@@ -11,10 +12,10 @@ import (
 func main() {
 	today := godate.Today()
 	// 每周五添加下周的，方便计划下周的工作安排
-	//if today.Weekday() != godate.Friday {
-	//	log.Println("今天不是星期五")
-	//	os.Exit(0)
-	//}
+	if today.Weekday() != godate.Friday {
+		log.Println("今天不是星期五")
+		os.Exit(0)
+	}
 
 	monday := today.NextWorkday()
 	friday, _ := monday.AddDay(4)
