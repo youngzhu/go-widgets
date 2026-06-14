adc2f36edaf1c821219e144e5ca10d9a9acda79a
youngzy <young.zhu.2012@gmail.com>
2026-06-14 10:30:05 +0800

    Update salaryAward function to trigger on the 1st

--- coupon-reminder/bocom/bocom.go
+++ coupon-reminder/bocom/bocom.go
@@ -10,18 +10,21 @@ type Reminder struct{}
 
 func (r Reminder) Remind() {
 	checkin()
-	//salaryAward()
+	salaryAward()
 }
 
 var today = reminder.Today
 
 // 每月10号领20-10券
 // 工资卡奖励
+//
+// 开薪星期五
+// 每月（周五？）一次，领贴金券
 func salaryAward() {
-	if today.Day() == 10 {
+	if today.Day() == 1 {
 		log.Println("交行工资卡奖励")
-		dueOn, _ := today.AddDay(30)
-		reminder.CreateTodo("交行储蓄卡20-10", dueOn, today)
+		dueOn, _ := today.AddDay(15)
+		reminder.CreateTodo("交行储蓄卡-贴金券", dueOn, today)
 	}
 }
 
